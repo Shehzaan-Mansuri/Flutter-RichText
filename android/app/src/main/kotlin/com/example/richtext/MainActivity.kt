4ae46948e334b9f1dd215885053900a375d3e732
Shehzaan-Mansuri <shehzaanmansuri1@gmail.com>
2021-08-31 20:30:06 -0700

    Flutter RichText

---
+++ android/app/src/main/kotlin/com/example/richtext/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.richtext
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: RichText(
+            text: TextSpan(
+              text: 'Hello ',
+              style: DefaultTextStyle.of(context).style,
+              children: const <TextSpan>[
+                TextSpan(text: 'bold', style: TextStyle(fontWeight: FontWeight.bold)),
+                TextSpan(text: ' world!'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
